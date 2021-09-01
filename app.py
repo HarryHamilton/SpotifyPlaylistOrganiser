5072c5e7e2c5597735b23da29b3032a5a1a0f7ad
Harry <b9026715@ncl.ac.uk>
2021-09-01 12:21:38 +0100

    Implemented token data verification.
    The code now checks if token data exists and if it is valid. If not, it will
    either ask the user to login or will attempt to refresh the token if the
    user is already logged in.

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import time
+
 from flask import Flask, request, url_for, session, redirect
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
@@ -6,6 +8,7 @@ import secrets
 app = Flask(__name__)
 app.secret_key = ""  # Random string used to sign the session
 app.config["SESSION_COOKIE_NAME"] = "Spotify Playlist Organiser"
+TOKEN_INFO = "token_info"
 
 
 # Sets up pages/endpoints for the website
@@ -20,13 +23,42 @@ def redirectPage():
     """This will be where the user is redirected to after
     completing the OAuth
     """
-
-    return "redirect"
+    sp_oath = create_spotify_oauth()
+    session.clear()
+    code = request.args.get("code")
+    token_info = sp_oath.get_access_token(code)
+    session[TOKEN_INFO] = token_info
+    return redirect(url_for("getTracks", _external=True),)
 
 
 @app.route("/getTracks")
 def getTracks():
-    return "Get tracks page"
+    try:
+        token_info = get_token()
+    except:
+        print("User not logged in")
+        redirect("/")  # Redirect user to login page if they aren't logged in
+
+
+def get_token():
+    """ Ensures valid token data exists.
+    - Check that is hasn't expired. If it has, refresh it.
+    - If token data never existed (e.g. user never signed in), redirect
+    the user to the login page.
+    """
+    # Checks if token data exists (i.e. if user is logged in or not)
+    token_info = session.get(TOKEN_INFO, None)  # "None" will be stored if no token data exists
+    if not token_info:
+        raise "exception"
+
+    # If token data exists, check that it hasn't expired. Refresh the token if it has
+    now = int(time.time())
+    is_expired = token_info["expires_at"] - now < 60
+    if is_expired:
+        sp_oath = create_spotify_oauth()
+        token_info = sp_oath.get_refresh_access_token(token_info["refresh_token"])
+    return token_info
+
 
 
 def create_spotify_oauth():
