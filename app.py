b9036d128f0b41c7c7a20382891ba25ce36df8d3
Harry <b9026715@ncl.ac.uk>
2021-09-26 11:04:30 +0100

    TEST COMMIT IGNORE

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import pprint
 import secrets
 
 app = Flask(__name__)
-app.secret_key = ""  # Random string used to sign the session
+app.secret_key = "segerghergerg"  # Random string used to sign the session
 app.config["SESSION_COOKIE_NAME"] = "Spotify Playlist Organiser"
 TOKEN_INFO = "token_info"
 
@@ -18,6 +18,7 @@ def login():
     sp_oath = create_spotify_oauth()
     auth_url = sp_oath.get_authorize_url()
     return redirect(auth_url)
+
 
 @app.route("/redirect")
 def redirectPage():
@@ -33,7 +34,6 @@ def redirectPage():
     return redirect(url_for("getTracks", _external=True), )
 
 
-
 @app.route("/getTracks")
 def getTracks():
     try:
@@ -43,19 +43,51 @@ def getTracks():
         return redirect("/")  # Redirect user to login page if they aren't logged in
 
     sp = spotipy.Spotify(auth=token_info["access_token"])
+    current_user = sp.current_user()
+    current_user_uri = current_user["uri"]
 
-    result = sp.current_user_playlists(limit=50)
-    users_saved_followed_playlists = []  # Array of playlist URI's that the user follows/owns
-    while result:
-        for i, playlist in enumerate(result['items']):
-            print("%4d %s %s" % (i + 1 + result['offset'], playlist['uri'], playlist['name']))
-            users_saved_followed_playlists.append(playlist['uri'])
-        if result['next']:
-            result = sp.next(result)
+    # Gets users saved/owned playlists and stores them
+    all_playlists = sp.current_user_playlists(limit=50)
+    uris_of_saved_followed_playlists = []  # Array of playlist URI's that the user follows/owns
+    while all_playlists:
+        for i, playlist in enumerate(all_playlists['items']):
+            #print("%4d %s %s" % (i + 1 + all_playlists['offset'], playlist['uri'], playlist['name']))
+            uris_of_saved_followed_playlists.append(playlist['uri'])
+        if all_playlists['next']:
+            all_playlists = sp.next(all_playlists)
         else:
-            result = None
-    print(users_saved_followed_playlists)
+            break
+
+    for current_playlist_uri in uris_of_saved_followed_playlists:
+        current_playlist_owner = (sp.playlist(playlist_id=current_playlist_uri, fields="owner.uri"))  # Retrieves URI of owner of playlist
+        print("hi")
+        if str(current_playlist_owner) != ("{'owner': {'uri': '%s'}" % current_user_uri + "}"):
+            #print("Len before: ")
+            #print(len(uris_of_saved_followed_playlists))
+            uris_of_saved_followed_playlists.remove(str(current_playlist_uri))
+            #print("Len after: ")
+            #print(len(uris_of_saved_followed_playlists))
+
+    # Prints the names of the owners of the final playlists, for testing purposes to check if the code block
+    # above is working correctly
+    #for x in uris_of_saved_followed_playlists:
+        #print("Len final: ")
+        #print(len(uris_of_saved_followed_playlists))
+        #print(sp.playlist(playlist_id=x, fields="owner.uri"))
+
     return "results in console"
+
+    # To work on:
+    # Struggling to get the program to delete the playlists that the user doesn't own from the master array.
+    # I think the problem is with the .remove line, as the code DOES seem to be entering the if statement block
+    # When looking at the length of the array that stores ALL playlists (followed/created), it has 132 elements,
+    # which is the correct number of playlists that i do follow/own.
+    # However, after the code has ran, the list that is supposed to store just the playlists I OWN has 99 elements,
+    # when is should be 79. This tells me that not ALL playlists are being removed. The code must not be
+    # Looping through the master list enough. Testing shows it is looping through the list 99 times, when in fact
+    # it should be 130 something times.
+    # I dont think it is anything to do with the spotify API, as I am getting a '200' response, which indicates
+    # that the request was carried out successfully.
 
 
 def get_token():
@@ -78,11 +110,10 @@ def get_token():
     return token_info
 
 
-
 def create_spotify_oauth():
     return SpotifyOAuth(
-        client_id="",
-        client_secret="",
+        client_id="a98aa14f0ffc4cff901798ddc9014476",
+        client_secret="d99e0b64d1fe43e1926ceb97d6a0ccf8",
         redirect_uri=url_for("redirectPage", _external=True),
         scope="ugc-image-upload, user-library-read, playlist-modify-private, playlist-read-private, "
               "playlist-modify-public, "
